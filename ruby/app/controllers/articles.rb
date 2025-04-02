f05e3b67b39cbf996a47b1c9420d646fa5032a87
Kiran N <kiran@catchmedia.com>
2025-04-02 15:12:57 +0200

    fix: ruby development

--- ruby/app/controllers/articles.rb
+++ ruby/app/controllers/articles.rb
@@ -1,13 +1,13 @@
 class ArticleController
   def create_article(article)
-    article_not_exists = ! (Article.where(:title => article['title']).empty?)
+    article_not_exists =  (Article.where(:title => article['title']).empty?)
 
     return { ok: false, msg: 'Article with given title already exists' } unless article_not_exists
 
     new_article = Article.new(:title => article['title'], :content => article['content'], :created_at => Time.now)
     new_article.save
 
-    { ok: false, obj: article }
+    { ok: true, obj: article }
   rescue StandardError
     { ok: false }
   end
@@ -16,13 +16,13 @@ class ArticleController
 
     article = Article.where(id: id).first
 
-    return { ok: false, msg: 'Article could not be found' } unless article.nil?
+    return { ok: false, msg: 'Article could not be found' } unless !article.nil?
 
     article.title = new_data['title']
     article.content = new_data['content']
-    article.save_changes
+    article.save
 
-    { ok: true }
+    { ok: true , obj: article}
   rescue StandardError
     { ok: false }
   end
@@ -31,25 +31,33 @@ class ArticleController
     res = Article.where(:id => id)
 
     if res.empty?
-      { ok: true, data: res }
-    else
       { ok: false, msg: 'Article not found' }
+    else
+      { ok: true, data: res.first }
     end
   rescue StandardError
     { ok: false }
   end
 
-  def delete_article(_id)
-    delete_count = Article.delete(:id => id)
+  def delete_article(id)
+    delete_count = Article.where(:id => id).delete_all
 
     if delete_count == 0
-      { ok: true }
+      { ok: false , msg: 'Article does not exist' }
     else
       { ok: true, delete_count: delete_count }
     end
   end
 
   def get_batch
-    
+    res = Article.all
+
+    if res.empty?
+      { ok: false, msg: 'No Articles found' }
+    else
+      { ok: true, data: res }
+    end
+  rescue StandardError
+    { ok: false }
   end
 end
